2c9a783b849cb6595439b427327d156b00b94def
lukasPokryvka24 <87475342+lukasPokryvka24@users.noreply.github.com>
2022-05-16 11:26:37 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./styles/App.css";
-import add from "./math";
+import "./styles/Header.css";
+import "./styles/List.css";
 
-console.log(add(2, 5));
+const todos = ["My first TODO", "My second TODO"];
+
+function getInputValue(event) {
+  let inputValue = event.target.value;
+  console.log(inputValue);
+}
+
+todos.forEach(renderTodos);
+
+function renderTodos(todo) {
+  const list = document.getElementById("list");
+  const listItem = document.createElement("li");
+  list.appendChild(listItem);
+  listItem.innerHTML = todo;
+}
+
+window.getInputValue = getInputValue;
